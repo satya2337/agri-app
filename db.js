a4e48151503e935cbc7fcef8b26fa4794f0b6758
Satya Anand <satyaanand442@gmail.com>
2025-11-15 20:57:30 +0530

    Updated db.js for Railway

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "agri_app",
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = pool;  // <- NOT pool.promise()
+module.exports = db;
